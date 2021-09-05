385cba0668355c1e32dbbe1417b31c8b9f981871
amittexts <amit.texts@gmail.com>
2021-09-05 13:19:12 +0530

    updated nodemon start

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const http = require('http')
+const lod = require('lodash')
 
+var items = [1,[2,[4,[4,5,6]]]]
 
-var server = http.createServer((req, res)=>{
-    if(req.url==='/'){
-        res.end('Welcome to Home page')
-    }else if(req.url==='/contactus'){
-        res.end('Welcome to contact us page')
-    }else{
-        res.end(`<h1>404 Error</h1>    
-        <a href='/'><button>home</button></a>`)       
-    }   
-})
-server.listen(3000)
+const newItems = lod.flattenDeep(items)
+console.log('comment')
+
+console.log(newItems)
